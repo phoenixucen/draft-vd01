daf72cfac65b4cb4a0fd576f42c7c49f01d2fd61
phoenixucen <130236440+phoenixucen@users.noreply.github.com>
2023-08-19 14:26:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("wipe-text").style.animation= "wipeout 1s linear 2.1s";
+const texts=["RIDE","OWER","URPOSE","RINCIPLE","REPARATION","LANNING","RACTICE","OINT","RODUCTIVITY","ATIENCE","ERSEVERANCE","HOENIX"];
+let i=0;
+setInterval(
+        () => {
+        if (i < texts.length) {
+                document.getElementById("wipe-text").innerHTML = texts[i];
+                document.getElementById("wipe-text").style.animation = "wipein 1s linear,wipeout 1s linear 2s";
+                setTimeout(() => {
+
+                        document.getElementById("wipe-text").innerHTML = "";
+                        document.getElementById("wipe-text").style.animation = "";
+                }, 3000); i++;
+        } else {
+                i = 0;
+                document.getElementById("wipe-text").innerHTML="HOENIX";
+        }
+
+},3100);
