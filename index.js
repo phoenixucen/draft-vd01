aa4b0429fb2af8149f81168b874c8d1f31df1ffb
phoenixucen <130236440+phoenixucen@users.noreply.github.com>
2023-08-19 15:12:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-document.getElementById("wipe-text").style.animation= "wipeout 1s linear 2.1s";
+
 const texts=["RIDE","OWER","URPOSE","RINCIPLE","REPARATION","LANNING","RACTICE","OINT","RODUCTIVITY","ATIENCE","ERSEVERANCE","HOENIX"];
 let i=0;
 setInterval(
         () => {
         if (i < texts.length) {
+                if(document.getElementById("wipe-text").innerHTML == "HOENIX"){
+                        for(let o = 0; o < document.getElementsByClassName("orange").length ; o++ ){
+                                document.getElementsByClassName("orange")[o].style.color = "#FD841F";
+                        }
+                }
                 document.getElementById("wipe-text").innerHTML = texts[i];
                 document.getElementById("wipe-text").style.animation = "wipein 1s linear,wipeout 1s linear 2s";
                 setTimeout(() => {
@@ -11,9 +16,11 @@ setInterval(
                         document.getElementById("wipe-text").innerHTML = "";
                         document.getElementById("wipe-text").style.animation = "";
                 }, 3000); i++;
+                
         } else {
                 i = 0;
                 document.getElementById("wipe-text").innerHTML="HOENIX";
         }
 
 },3100);
+
