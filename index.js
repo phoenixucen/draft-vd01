056b0536cafa689f5812ce62a4f4b015856e398d
phoenixucen <130236440+phoenixucen@users.noreply.github.com>
2023-08-19 15:27:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,25 @@ document.getElementById("wipe-text").style.animation= "wipeout 1s linear 2.1s";
 const texts=["RIDE","OWER","URPOSE","RINCIPLE","REPARATION","LANNING","RACTICE","OINT","RODUCTIVITY","ATIENCE","ERSEVERANCE","HOENIX"];
 let i=0;
 setInterval(
-        () => {
-         if(document.getElementById("wipe-text").innerHTML == "HOENIX"){
-                for(let o = 0; o < document.getElementsByClassName("orange").length ; o++ ){
-                         document.getElementsByClassName("orange")[o].style.color = "#FD841F";
-                }
-        }else{
-                for(let o = 0; o < document.getElementsByClassName("orange").length ; o++ ){
-                         document.getElementsByClassName("orange")[o].style.color = "white";
-                }
-         }       
+        () => {      
         
         if (i < texts.length) {
                 document.getElementById("wipe-text").innerHTML = texts[i];
+                if (i==11)
+                {
+                        for(let o = 0; o < document.getElementsByClassName("orange").length ; o++ ){
+                                 document.getElementsByClassName("orange")[o].style.color = "#FD841F";
+                        }
+                }
                 document.getElementById("wipe-text").style.animation = "wipein 1s linear,wipeout 1s linear 2s";
                 setTimeout(() => {
 
                         document.getElementById("wipe-text").innerHTML = "";
                         document.getElementById("wipe-text").style.animation = "";
                 }, 3000); i++;
-                
+                for(let o = 0; o < document.getElementsByClassName("orange").length ; o++ ){
+                         document.getElementsByClassName("orange")[o].style.color = "white";
+                }
         } else {
                 i = 0;
                 document.getElementById("wipe-text").innerHTML="HOENIX";
